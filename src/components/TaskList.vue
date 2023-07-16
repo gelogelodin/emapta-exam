315659db3b4cfa06ebb0110683ba53bd59f6d288
gelogelodin <bantiling.angelo@gmail.com>
2023-07-16 17:13:31 +0800

    Feature: added mark checkbox as complete

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { getTaskList, deleteTask } from '../services/taskService';
+import { getTaskList,updateTask, deleteTask } from '../services/taskService';
 import { statusEnum } from '../constants/statusEnum';
 
 export default {
@@ -36,6 +36,18 @@ export default {
           this.getTasks();
         });
       }
+    },
+    markComplete: function(task:any){
+      const formData = {
+        ...task,
+        state: 'completed',
+      };
+      
+      updateTask(formData, task.task_id).then((response) => {
+          alert('Task ' + task.name +' marked completed!');
+          console.log(response);
+          this.getTasks();
+        });
     }
   },
   mounted() {
@@ -54,6 +66,9 @@ export default {
     </div>
     <div class="grid grid-cols-4">
       <a v-for="task in taskList" href="#" class="block mb-5 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center mb-4">
+          <input @click="markComplete(task)" :disabled="task.state === 'completed'" id="default-checkbox" type="checkbox" :checked="task.state === 'completed'" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 disabled:text-blue-600">
+        </div>
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ task.name }}</h5>
           <div class="text-white w-1/3 ml-auto mr-auto mb-2 uppercase" :class="{ 'bg-orange-600': task.state === 'pending',
                          'bg-blue-600': task.state === 'in progress',
